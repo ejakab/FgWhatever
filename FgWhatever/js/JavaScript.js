e274a89092013e735156200ff3cac8f8a22da0a4
ejakab <jakab_edina@yahoo.com>
2017-11-06 19:40:11 +0100

    styling of schedule

---
+++ FgWhatever/js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+
+    //Show Banner
+    $(".main_image .desc").show(); //Show Banner
+    
+
+
+    //Toggle Teaser
+    $("a.collapse").click(function () {
+        $(this).next().slideToggle();
+        $(this).toggleClass("show");
+    });
+
+
+});//Close Function  
